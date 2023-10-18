eadc8a41d7895bd22f1730b689bd049bdd11db17
Tami Tubul <tamit0430@gmail.com>
2023-10-18 10:57:06 +0300

    save changes

--- src/components/WeatherComp.js
+++ src/components/WeatherComp.js
@@ -5,18 +5,25 @@ import { getTodayDate } from "../todayDate";
 const WeatherComp = ({ city }) => {
 
     const [weatherObj, setWeatherObj] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [message, setMessage] = useState("טוען נתונים...");
+
     const cityRef = useRef();
 
     const searchCityWeather = async (e) => {
         e.preventDefault();
         let cityName = cityRef.current.value;
+        if (!cityName)
+            setMessage("יש להקליד עיר/ארץ בשדה החיפוש")
         city(cityName);
         try {
+            setLoading(true);
             const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=64cd513da356063b49bc3bdd79b3742e&units=metric&lang=he`);
             setWeatherObj(resp.data);
+            setLoading(false);
         } catch (error) {
             if (error.response.status === 404)
-                alert("עיר/ארץ לא נמצאת")
+                setMessage("עיר/ארץ לא נמצאת")
         }
     }
 
@@ -24,6 +31,7 @@ const WeatherComp = ({ city }) => {
         async function getDataFromServer() {
             const resp = await axios.get("https://api.openweathermap.org/data/2.5/weather?q=Jerusalem&appid=64cd513da356063b49bc3bdd79b3742e&units=metric&lang=he");
             setWeatherObj(resp.data);
+            setLoading(false);
         }
         getDataFromServer();
     }, [])
@@ -36,13 +44,16 @@ const WeatherComp = ({ city }) => {
                 <input type="search" ref={cityRef} placeholder="הקלד שם עיר/ארץ" />
                 <button type="submit" onClick={searchCityWeather}>חפש</button>
             </div>
-            <div>{weatherObj.weather && weatherObj.weather.map((x, index) => {
-                return <div key={index}>
-                    <p>{x.description}</p>
-                    <p>{weatherObj.main.temp_max}° - {weatherObj.main.temp_min}°</p>
-                    <img src={`/assets/${x.main}.png`} alt={x.description} width={200} />
-                </div>
-            })}</div>
+            {!loading ?
+                <div>{weatherObj.weather && weatherObj.weather.map((x, index) => {
+                    return <div key={index}>
+                        <p>{x.description}</p>
+                        <p>{weatherObj.main.temp_max}° - {weatherObj.main.temp_min}°</p>
+                        <img src={`/assets/${x.main}.png`} alt={x.description} width={200} />
+                    </div>
+                })}</div>
+                :
+                <p>{message}</p>}
         </>
     )
 }
